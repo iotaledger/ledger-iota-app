75077d14f644df2dde27ef45cf174de9890dc82f
shufps <shufps80@gmail.com>
2021-02-25 10:05:30 +0100

    removed chmod from Dockerfile and moved it to run_tests_docker.sh

--- tests/run_tests_docker.sh
+++ tests/run_tests_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo
 echo "testing NANO S"
-docker run -it test-app bash -c "cd /root/git/ledger-iota-app/tests; ./test_headless.sh -m nanos" || exit 1
+docker run -it test-app bash -c "cd /root/git/ledger-iota-app/tests; chmod a+x test_headless.sh; ./test_headless.sh -m nanos" || exit 1
 echo
 echo "testing NANO X"
-docker run -it test-app bash -c "cd /root/git/ledger-iota-app/tests; ./test_headless.sh -m nanox" || exit 1
+docker run -it test-app bash -c "cd /root/git/ledger-iota-app/tests; chmod a+x test_headless.sh; ./test_headless.sh -m nanox" || exit 1
