0cef61f2b096225b6f1fc2efb0f0c5129eb27d72
Edouard Merle <edouard.merle@ledger.fr>
2022-06-13 14:29:56 +0200

    fix: disable Wpedantic to build on newer lns+ sdks

--- src/api.c
+++ src/api.c
@@ -12,7 +12,7 @@
 
 // gcc doesn't know this and ledger's SDK cannot be compiled with Werror!
 //#pragma GCC diagnostic error "-Werror"
-#pragma GCC diagnostic error "-Wpedantic"
+//#pragma GCC diagnostic error "-Wpedantic"
 #pragma GCC diagnostic error "-Wall"
 #pragma GCC diagnostic error "-Wextra"
 #pragma GCC diagnostic error "-Wmissing-prototypes"
--- src/ui/nano/flow_user_confirm.c
+++ src/ui/nano/flow_user_confirm.c
@@ -19,7 +19,7 @@
 
 // gcc doesn't know this and ledger's SDK cannot be compiled with Werror!
 //#pragma GCC diagnostic error "-Werror"
-#pragma GCC diagnostic error "-Wpedantic"
+//#pragma GCC diagnostic error "-Wpedantic"
 #pragma GCC diagnostic error "-Wall"
 #pragma GCC diagnostic error "-Wextra"
 
