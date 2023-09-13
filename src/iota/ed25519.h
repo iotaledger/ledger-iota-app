eba8048dfeb8f1567a9aa88ab6df57388a89ed45
shufps <shufps80@gmail.com>
2023-09-13 11:45:07 +0200

    linter

--- src/iota/ed25519.h
+++ src/iota/ed25519.h
@@ -2,4 +2,5 @@
 
 #include "os.h"
 
-uint8_t ed25519_public_key_to_bytes(const uint8_t raw_pubkey[65], uint8_t output[32]);
+uint8_t ed25519_public_key_to_bytes(const uint8_t raw_pubkey[65],
+                                    uint8_t output[32]);
